a1b96c6fd80a6afc4e9f8b83afb0d69f40697772
Gilson Doi Junior <doijunior@yahoo.com.br>
2021-11-27 23:21:13 -0300

    updated solana contract program for devnet

--- myepicproject/programs/myepicproject/src/lib.rs
+++ myepicproject/programs/myepicproject/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("HQ5tDbxCGvvAdSbsqykXg7Enc1odWVasRCA5xPvyYVaj");
 
 #[program]
 pub mod myepicproject {
